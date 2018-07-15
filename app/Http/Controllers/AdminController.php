b45586d313b869820be29d139e68edd061eea059
= <=>
2018-07-15 12:25:06 +0200

    user

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -27,7 +27,7 @@ class AdminController extends Controller
 
     public function __construct()
     {
-        $this->middleware('administrator', ['except' => ['isAdministrator']]);
+        $this->middleware('voten-administrator', ['except' => ['isAdministrator']]);
     }
 
     public function activities()
