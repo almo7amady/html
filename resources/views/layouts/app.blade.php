3693c4f1ab7c5614e8e5d1913aedf8c124e371c6
= <=>
2018-07-20 10:31:56 +0200

    cdn

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     @yield('head')
-    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+    <!--<link rel="stylesheet" href="{{ mix('/css/app.css') }}">-->
+    <link rel="stylesheet" href="https://caseroot.ams3.digitaloceanspaces.com/cdn/app.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/emojione/2.2.7/lib/js/emojione.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/emojione/2.2.7/assets/css/emojione.min.css"/>
    
@@ -64,9 +65,14 @@
 @include('php-to-js-data')
 
 @yield('script')
-	<script src="{{ mix('/js/manifest.js') }}"></script>
+	<!--
+    <script src="{{ mix('/js/manifest.js') }}"></script>
 	<script src="{{ mix('/js/vendor.js') }}"></script>
 	<script src="{{ mix('/js/app.js') }}"></script>
+    -->
+	<script src="https://caseroot.ams3.digitaloceanspaces.com/cdn/manifest.js"></script>
+	<script src="https://caseroot.ams3.digitaloceanspaces.com/cdn/vendor.js"></script>
+	<script src="https://caseroot.ams3.digitaloceanspaces.com/cdn/app.js"></script>
 @yield('footer')
 
 <!-- Twitter universal website tag code
