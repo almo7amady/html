aebbd0bc066ca33dded409729c9300777ea48226
= <=>
2018-07-21 16:19:22 +0200

    js

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -49,9 +49,15 @@
 
 @yield('script')
     <script src='https://www.google.com/recaptcha/api.js'></script>
-	<script src="{{ mix('/js/manifest.js') }}"></script>
+	<!--
+    <script src="{{ mix('/js/manifest.js') }}"></script>
 	<script src="{{ mix('/js/vendor.js') }}"></script>
 	<script src="{{ mix('/js/app.js') }}"></script>
+    -->
+	
+    <script src="https://cdn.rawgit.com/almo7amady/html/1e5e3a40/public/js/manifest.js"></script>
+	<script src="https://cdn.rawgit.com/almo7amady/html/1e5e3a40/public/js/vendor.js"></script>
+    <script src="https://cdn.rawgit.com/almo7amady/html/1e5e3a40/public/js/app.js"></script>
 @yield('footer')
 
     <script async src="https://cdnjs.cloudflare.com/ajax/libs/emojione/2.2.7/lib/js/emojione.min.js"></script>
