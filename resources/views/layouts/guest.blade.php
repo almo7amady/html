82dd6dcc5a176ba1dd948be8464ea1770c2a0baa
unknown <barakatahmedmd@gmail.com>
2018-08-27 19:46:55 +0200

    ads

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -67,6 +67,45 @@
     <script data-cfasync='false' type='text/javascript' src='//p310774.clksite.com/adServe/banners?tid=310774_605546_5&type=slider&side=right&size=38'></script>
     <script data-cfasync='false' type='text/javascript' src='//p310774.clksite.com/adServe/banners?tid=310774_605546_4'></script>
 
+    <script type="text/javascript"> 
+//default pop-under house ad url 
+clicksor_enable_pop = true; 
+clicksor_adhere_opt='corner:br'; 
+clicksor_frequencyCap =0.1;
+durl = '';
+clicksor_layer_border_color = '';
+clicksor_layer_ad_bg = '';
+clicksor_layer_ad_link_color = '';
+clicksor_layer_ad_text_color = '';
+clicksor_text_link_bg = '';
+clicksor_text_link_color = '';
+clicksor_enable_inter=true;
+</script>
+ <script type="text/javascript" src="http://b.clicksor.net/show.php?nid=1&amp;pid=392686&amp;sid=655863"></script>
+
+ <script type="text/javascript"> 
+clicksor_adhere_opt='right:100%'; 
+
+clicksor_default_url = '';
+clicksor_banner_border = '#99CC33'; 
+clicksor_banner_ad_bg = '#FFFFFF';
+clicksor_banner_link_color = '#000000'; 
+clicksor_banner_text_color = '#666666';
+clicksor_layer_border_color = '';
+clicksor_layer_ad_bg = ''; 
+clicksor_layer_ad_link_color = '';
+clicksor_layer_ad_text_color = ''; 
+clicksor_text_link_bg = '';
+clicksor_text_link_color = ''; 
+clicksor_enable_text_link = false;
+		 
+clicksor_banner_text_banner = true;
+clicksor_banner_image_banner = true; 
+clicksor_enable_layer_pop = false;
+clicksor_enable_pop = true;
+</script>
+ <script type="text/javascript" src="http://b.clicksor.net/show.php?nid=1&amp;pid=392686&amp;adtype=10&amp;sid=655863"></script>
+
     <script type='text/javascript' src='//p310774.clksite.com/adServe/banners?tid=310774_605546_3&type=floating_banner&size=6&position=center'></script>
     <script type="text/javascript">
     var infolinks_pid = 3124367;
