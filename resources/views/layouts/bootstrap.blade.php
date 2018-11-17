04e5f2d7c793b3007749c0b9a483c605d9d82140
unknown <barakatahmedmd@gmail.com>
2018-11-18 00:58:13 +0200

    gitcdn

--- resources/views/layouts/bootstrap.blade.php
+++ resources/views/layouts/bootstrap.blade.php
@@ -20,13 +20,13 @@
     </div>
 
     @include('php-to-js-data') @yield('script')
-   <!--
     <script src="{{ mix('/js/manifest.js') }}"></script>
 	<script src="{{ mix('/js/vendor.js') }}"></script>
-    -->
+   <!--
 
     <script src="https://cdn.jsdelivr.net/gh/almo7amady/html/master/public/js/manifest.js"></script>
 	<script src="https://cdn.jsdelivr.net/gh/almo7amady/html/master/public/js/vendor.js"></script>
+        -->
     <script src="{{ mix('/js/bootstrap4.js') }}"></script>
     @yield('footer')
     <script src="https://code.jquery.com/jquery-3.3.2.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
